9f2ab5748f8e14446202f701d0fd706b91f8b900
W1z4r3 <153330055+W1z4r3@users.noreply.github.com>
2023-12-08 17:51:45 -0500

    Create Autoclave.js

---
+++ js/Autoclave.js
@@ -0,0 +1,49 @@
+function cifrarAutoclave(texto, clave) {
+  texto = texto.toUpperCase();
+  clave = clave.toUpperCase();
+
+  let resultado = '';
+
+  for (let i = 0; i < texto.length; i++) {
+    let char = texto.charAt(i);
+    let charCode = texto.charCodeAt(i);
+
+    if (charCode >= 65 && charCode <= 90) {
+      let claveChar = clave.charAt(i % clave.length);
+      let claveCode = claveChar.charCodeAt(0) - 65;
+
+      let nuevoCharCode = ((charCode - 65 + claveCode) % 26) + 65;
+
+      resultado += String.fromCharCode(nuevoCharCode);
+    } else {
+      resultado += char;
+    }
+  }
+
+  return resultado;
+}
+
+function descifrarAutoclave(textoCifrado, clave) {
+  textoCifrado = textoCifrado.toUpperCase();
+  clave = clave.toUpperCase();
+
+  let resultado = '';
+
+  for (let i = 0; i < textoCifrado.length; i++) {
+    let char = textoCifrado.charAt(i);
+    let charCode = textoCifrado.charCodeAt(i);
+
+    if (charCode >= 65 && charCode <= 90) {
+      let claveChar = clave.charAt(i % clave.length);
+      let claveCode = claveChar.charCodeAt(0) - 65;
+
+      let nuevoCharCode = ((charCode - 65 - claveCode + 26) % 26) + 65;
+
+      resultado += String.fromCharCode(nuevoCharCode);
+    } else {
+      resultado += char;
+    }
+  }
+
+  return resultado;
+}
